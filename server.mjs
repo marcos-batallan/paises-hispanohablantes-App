fdf443136d1e27d0f94ce2222e173ab54fb7d28b
Marcos Batallan <sickness.meb@gmail.com>
2026-05-21 00:32:02 -0300

    Avance del proyecto con implementación de express-ejs-layuots

--- server.mjs
+++ server.mjs
@@ -3,17 +3,21 @@ import app from './src/app.mjs';
 import connectDB from './src/config/dbConfig.mjs';
 import { getSpanishSpeakingCountries } from './src/services/countriesService.mjs';
 
+// Puerto en el que se ejecutará el servidor
 const PORT = process.env.PORT || 3000;
 
+// Conectar a la base de datos antes de iniciar el servidor
 connectDB();
 
+// Iniciar el servidor
 app.listen(PORT, () => {
   console.log(`Servidor escuchando en el puerto ${PORT}`);
 });
 
+// Función de prueba para verificar que la función getSpanishSpeakingCountries funciona correctamente
 const test = async () => {
   const data = await getSpanishSpeakingCountries();
-  console.log(data);
+  console.log(`Se cargaron ${data.length} países`);
 };
 
 test();
--- src/app.mjs
+++ src/app.mjs
@@ -1,10 +1,19 @@
 import express from 'express';
 import path from 'path';
+import { fileURLToPath } from 'url';
 import morgan from 'morgan';
 import methodOverride from 'method-override';
-import countryRoutes from './routes/countryRoutes.mjs';
+import expressLayouts from 'express-ejs-layouts';
+import countryRoutes from './routes/countriesRoutes.mjs';
 
+// Crear una instancia de Express
 const app = express();
+
+// Obtener el nombre del archivo y el directorio actual
+const __filename = fileURLToPath(import.meta.url);
+
+// Obtener el directorio actual
+const __dirname = path.dirname(__filename);
 
 // Configuración básica de Express
 app.use(express.json());
@@ -19,11 +28,24 @@ app.use(methodOverride('_method'));
 // Rutas para manejar las operaciones relacionadas con los países
 app.use('/countries', countryRoutes);
 
-// Motor de vistas
-app.set('view-engine', 'ejs');
-app.set('views', path.resolve('src/views'));
+// Configuración de EJS como motor de plantillas
+app.set('view engine', 'ejs');
+
+// Configuración de express-ejs-layouts para usar un layout principal
+app.use(expressLayouts);
+app.set('layout', 'layouts/main');
+
+// Configurar la carpeta de vistas
+app.set('views', path.join(__dirname, 'views'));
 
 // Archivos estáticos
-app.use(express.static('public'));
+app.use(express.static(path.join(__dirname, '../public')));
+
+// Ruta para la página de inicio
+app.get('/', (req, res) => {
+    res.render('home', {
+        title: 'Inicio'
+    });
+});
 
 export default app;
